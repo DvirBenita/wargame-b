a8a7b0a462ce70e9f4e98f2c80629d30d69a497a
DvirBenita <benitosh12@gmail.com>
2020-05-25 14:28:55 +0300

    fix Tests

--- Board.cpp
+++ Board.cpp
@@ -19,7 +19,7 @@ bool WarGame::Board::has_soldiers(uint player_number) const{
     for (int i = 0; i < board.size(); i++) {
         for (int j = 0; j < board[i].size(); j++) {
             if (board[i][j] != nullptr) {
-                if (board[i][j]->player_num == player_number) {
+                if (board[i][j]->player_number == player_number) {
                     return true;
                 }
             }
