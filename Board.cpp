5b049557f28637806015c255c9a1e5293c3f67b0
DvirBenita <benitosh12@gmail.com>
2020-05-25 14:28:00 +0300

    fix Tests

--- Board.cpp
+++ Board.cpp
@@ -15,5 +15,16 @@ void WarGame::Board:: move(uint player_number, std::pair<int,int> source, MoveDI
 }
 
 bool WarGame::Board::has_soldiers(uint player_number) const{
+  
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            if (board[i][j] != nullptr) {
+                if (board[i][j]->player_num == player_number) {
+                    return true;
+                }
+            }
+        }
+    }
     return false;
+
 }
