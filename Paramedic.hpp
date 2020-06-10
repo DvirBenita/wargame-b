eb7daf88dc42e47b58860723cb156ca32fe1caa6
Dvir Benita <benitosh12@gmail.com>
2020-06-11 00:30:35 +0300

    commit

--- Paramedic.hpp
+++ Paramedic.hpp
@@ -6,7 +6,7 @@
 class Paramedic : public Soldier{
 
      public:
-    Paramedic(int player_number):Soldier(player_number,paramedic_health){}
+    Paramedic(uint player_number):Soldier(player_number,paramedic_health){}
     void move(uint player_number,std::pair<int,int> source,std::vector<std::vector<Soldier*>>& board);
-  
+    int fullLife(){};
 };
